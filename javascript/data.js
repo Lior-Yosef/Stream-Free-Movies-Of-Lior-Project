1df7fabce12dc06096685b8c19f87eefe9a692c8
Lior-Yosef <lioryosef06@gmail.com>
2022-01-04 20:05:36 +0200

    add css tabel

--- javascript/data.js
+++ javascript/data.js
@@ -11,11 +11,12 @@ async function getData(id) {
   }
   let movieId = location.search.substr(4);
   getData(movieId).then((movie) => {
-    title.innerHTML = `<div id="card">
-      <h1>${movie.data.movieName} </h1>
-      <img id="image" src="${movie.data.image}">
-      <h3>${movie.data.rating}</h3>
-      <h3>${movie.data.synopsis} </h3>
+    title.innerHTML = `
+    <article id="imgArticle"> <img id="image" src="${movie.data.image}"> </article>
+    <div id="card">
+    <h1>${movie.data.movieName} </h1>
+    <h3>${movie.data.synopsis} </h3>
+    <h3>${movie.data.rating}</h3>
       </div>
      `;
   });
